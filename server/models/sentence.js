ac496e6db2dcb1bdf62021bf519dd8009fb544d4
Burak Gündüz <burakkgunduz21@gmail.com>
2021-07-19 20:58:36 +0300

    project migrated to next.js

--- server/models/sentence.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const sentenceSchema = new Schema({
-    italian: String,
-    english: String,
-});
-
-module.exports = mongoose.model("Sentence", sentenceSchema);
