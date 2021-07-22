28beabfd2faaf787eb69a69e52f105c837c6cb44
Burak Gündüz <burakkgunduz21@gmail.com>
2021-07-22 17:44:14 +0300

    next-transpile-modules has been removed

--- next.config.js
+++ next.config.js
@@ -1,22 +1,17 @@
 const withImages = require("next-images");
-const withTM = require("next-transpile-modules")(["flagpack-core"]);
-const path = require("path");
 
-module.exports = withTM(
-  withImages({
-    reactStrictMode: true,
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/i,
-        include: [path.resolve(__dirname, "/node_modules/flagpack-core")],
-        issuer: { and: [/\.(js|ts|md)x?$/] },
-        use: [
-          {
-            loader: "@svgr/webpack",
-          },
-        ],
-      });
-      return config;
-    },
-  })
-);
+module.exports = withImages({
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      use: [
+        {
+          loader: "@svgr/webpack",
+        },
+      ],
+    });
+    return config;
+  },
+});
