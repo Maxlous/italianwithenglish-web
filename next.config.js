3a1b50e8e8414a0432493563b3973aa4b26bb8c3
Burak Gündüz <burakkgunduz21@gmail.com>
2021-07-22 19:16:46 +0300

    svgr-webpack removed from project

--- next.config.js
+++ next.config.js
@@ -2,16 +2,4 @@ const withImages = require("next-images");
 
 module.exports = withImages({
   reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: { and: [/\.(js|ts|md)x?$/] },
-      use: [
-        {
-          loader: "@svgr/webpack",
-        },
-      ],
-    });
-    return config;
-  },
 });
