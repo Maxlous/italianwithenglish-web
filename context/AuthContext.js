852db21cae0c67829b4a20fb03db673a2eda69b4
Burak Gündüz <burakkgunduz21@gmail.com>
2021-08-15 19:40:37 +0300

    authcontext login function connects to next api in order to set http only cookie

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,5 +1,6 @@
 import { createContext, useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import { NEXT_URL } from "../config/index";
 
 const AuthContext = createContext();
 
@@ -9,7 +10,21 @@ export const AuthProvider = ({ children }) => {
 
   const register = async (user) => {};
 
-  const login = async ({ email: identifier, password }) => {};
+  const login = async ({ email: identifier, password }) => {
+    const res = await fetch(`${NEXT_URL}/api/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ identifier, password }),
+    });
+    const data = await res.json();
+
+    if (res.ok) {
+      setUser(data.user);
+    } else {
+      setError(data.message);
+      setError(null);
+    }
+  };
 
   const logout = async () => {};
 
