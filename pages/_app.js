cdc33721881f8509e432185bfd3a26cad30db208
Burak Gündüz <burakkgunduz21@gmail.com>
2021-11-14 11:45:16 +0300

    google analytics added to project1 y

--- pages/_app.js
+++ pages/_app.js
@@ -2,22 +2,58 @@ import { useEffect } from "react";
 import { ThemeProvider } from "../context/ThemeContext";
 import { AuthProvider } from "../context/AuthContext";
 import { ToastContainer } from "react-toastify";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   useEffect(() => {
     typeof document !== "undefined"
       ? require("bootstrap/dist/js/bootstrap.js")
       : null;
   }, []);
+
   return (
-    <AuthProvider>
-      <ThemeProvider>
-        <Component {...pageProps} />
-        <ToastContainer theme="dark" />
-      </ThemeProvider>
-    </AuthProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <AuthProvider>
+        <ThemeProvider>
+          <Component {...pageProps} />
+          <ToastContainer theme="dark" />
+        </ThemeProvider>
+      </AuthProvider>
+    </>
   );
 }
 
